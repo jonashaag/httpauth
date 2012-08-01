34053c507318b75799c2e00b5ed5b42fcdfb8106
Jonas Haag <jonas@lophus.org>
2012-08-01 20:52:35 +0200

    0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='httpauth',
-    version='0.1.1',
+    version='0.2',
     author='Jonas Haag',
     author_email='jonas@lophus.org',
     url='https://github.com/jonashaag/httpauth',
