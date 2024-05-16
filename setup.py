b96ec3d14ed1c3b6d7ee65a7babf81775d55817b
Jonas Haag <jonas@lophus.org>
2024-05-16 22:57:33 +0200

    Fix Python 3 support (#5)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='httpauth',
-    version='0.3',
+    version='0.4',
     author='Jonas Haag',
     author_email='jonas@lophus.org',
     url='https://github.com/jonashaag/httpauth',
@@ -12,10 +12,7 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ]
 )
